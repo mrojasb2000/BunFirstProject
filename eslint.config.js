2374eb86fee32cca12bc3e1cb13871cff39e6536
Mauricio Rojas <mrojasb2000@gmail.com>
2025-03-20 15:51:07 -0300

    chore: add custom config eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,32 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import js from "@eslint/js"
+import tsParser from "@typescript-eslint/parser"
+import unusedImports from "eslint-plugin-unused-imports"
+import airbnbBase from "eslint-config-airbnb-base"
+import airbnbBaseTypescript from "eslint-config-airbnb-base-typescript"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      "unused-imports": unusedImports
+    },
+    rules: {
+      ...airbnbBase.rules,
+      ...airbnbBaseTypescript.rules,
+      "no-unused-vars": "off"
+    }
+  },
 ];
